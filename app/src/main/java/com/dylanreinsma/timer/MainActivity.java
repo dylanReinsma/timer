ebf94f662792b64d652d88aa1c02ad9dbf4390f6
dylanreinsma <dylanreinsma@gmail.com>
2019-06-11 14:01:49 -0700

    update

--- app/src/main/java/com/dylanreinsma/timer/MainActivity.java
+++ app/src/main/java/com/dylanreinsma/timer/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,8 +13,31 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        SeekBar seekBar = findViewById(R.id.timerSeekBar);
-        seekBar.setProgress(0);
+        SeekBar timerSeekBar = findViewById(R.id.timerSeekBar);
+        TextView timerTextView = findViewById(R.id.timerTextView);
+
+        timerSeekBar.setMax(600);
+        timerSeekBar.setProgress(30);
+
+        timerSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                int minutes = progress / 60;
+                int seconds = progress - (minutes * 60);
+
+                timerTextView.setText(Integer.toString(minutes) + ":" + Integer.toString(seconds));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 
     public void timerClick() {
