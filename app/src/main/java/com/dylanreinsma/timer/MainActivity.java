d980b5bc65f85d5870fbc515e80d5a4b9e68ac5b
dylanreinsma <dylanreinsma@gmail.com>
2019-06-18 10:11:41 -0700

    update

--- app/src/main/java/com/dylanreinsma/timer/MainActivity.java
+++ app/src/main/java/com/dylanreinsma/timer/MainActivity.java
@@ -1,20 +1,53 @@
 package com.dylanreinsma.timer;
 
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView timerTextView;
+    SeekBar timerSeekBar;
+
+    public void buttonClick(View view) {
+        CountDownTimer countDownTimer = new CountDownTimer(timerSeekBar.getProgress() * 1000 + 100, 1000) {
+
+            @Override
+            public void onTick(long millisUntilFinished) {
+                updateTimer( (int)millisUntilFinished / 1000);
+            }
+
+            @Override
+            public void onFinish() {
+                Log.i("Finished", "Timer all done");
+            }
+        }.start();
+    }
+
+    public void updateTimer(int secondsLeft) {
+        int minutes = secondsLeft / 60;
+        int seconds = secondsLeft - (minutes * 60);
+
+        String secondString = Integer.toString(seconds);
+
+        if (seconds <= 9) {
+            secondString = "0" + secondString;
+        }
+
+        timerTextView.setText(Integer.toString(minutes) + ":" + secondString);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        SeekBar timerSeekBar = findViewById(R.id.timerSeekBar);
-        TextView timerTextView = findViewById(R.id.timerTextView);
+        timerSeekBar = findViewById(R.id.timerSeekBar);
+        timerTextView = findViewById(R.id.timerTextView);
 
         timerSeekBar.setMax(600);
         timerSeekBar.setProgress(30);
@@ -22,10 +55,7 @@ public class MainActivity extends AppCompatActivity {
         timerSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                int minutes = progress / 60;
-                int seconds = progress - (minutes * 60);
-
-                timerTextView.setText(Integer.toString(minutes) + ":" + Integer.toString(seconds));
+                updateTimer(progress);
             }
 
             @Override
